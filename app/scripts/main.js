11399d88541b1688e4776b5c943f96800be9e7a1
Fausto Perez <faustoperez@gmail.com>
2016-04-13 09:53:49 +0200

    demo code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,79 @@
-var fadeOut = {
+/************************
+		Element Variables
+************************/
 
-	// p is for properties
-	p: {
-		opacity: .2
-	},
+var $container = $("#container");
 
-	// o is for options
+/************************
+		Animation Setup
+************************/
 
-	o: {
-		duration: 1000,
-		easing: "linear"
-	}
-};
+/* Randomly generate an integer between two numbers. */
+function r (min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-$("body").on("click", function() {
-	$(".jumbotron").velocity(fadeOut.p, fadeOut.o);
-});
+/* Query the window’s dimensions. */
+var screenWidth = window.screen.availWidth,
+	screenHeight = window.screen.availHeight;
 
+/* Define our z-axis animation range. */
+var translateZMin = -725,
+	translateZMax = 600;
+
+/**********************
+		Circle Creation
+**********************/
+
+var circleCount = 500,
+	circlesHtml = "",
+	$circles = "";
+
+for (var i = 0; i < circleCount; i++) {
+	circlesHtml += "<div class='circle'></div>";
+}
+
+$circles = $(circlesHtml);
+
+/******************************
+		 Container Animation
+******************************/
+
+$container
+	.css("perspective-origin", screenWidth/2 + "px " + screenHeight/2 + "px")
+	.velocity(
+	{
+		perspective: [ 215, 50 ],
+		opacity: [ 0.90, 0.55 ]
+	}, {
+		duration: 800,
+		loop: 1,
+		delay: 3000
+	});
+
+/***************************
+		 Circles Animation
+***************************/
+
+$circles
+	.appendTo($container)
+	.velocity({
+		opacity: [
+			function() { return Math.random() },
+			function() { return Math.random() + 0.1 }
+		],
+		translateX: [
+			function() { return "+=" + r(-screenWidth/2.5, screenWidth/2.5) },
+			function() { return r(0, screenWidth) }
+		],
+		translateY: [
+			function() { return "+=" + r(-screenHeight/2.75, screenHeight/2.75) },
+			function() { return r(0, screenHeight) }
+		],
+		translateZ: [
+			function() { return "+=" + r(translateZMin, translateZMax) },
+			function() { return r(translateZMin, translateZMax) }
+		]
+	}, { duration: 6000 })
+	.velocity("reverse", { easing: "easeOutQuad" })
+	.velocity({ opacity: 0 }, 2000);
